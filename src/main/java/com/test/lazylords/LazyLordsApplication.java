5a621afe0eea773f879212440ffe25ed9116b255
Maksim Navoichyk <makseke@mail.ru>
2024-04-22 22:04:29 +0300

    base spring dependencies

---
+++ src/main/java/com/test/lazylords/LazyLordsApplication.java
@@ -0,0 +1,13 @@
+package com.test.lazylords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyLordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazyLordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/lazylords/LazyLordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.lazylords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazyLordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
